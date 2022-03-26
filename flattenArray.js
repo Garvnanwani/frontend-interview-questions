ef85296d4f179a89fe7020c3901b9e4b4db76384
garv nanwani <garvwashere@gmail.com>
2022-03-26 21:05:59 +0530

    flatten array implemented

---
+++ flattenArray.js
@@ -0,0 +1,18 @@
+// flattening an array means converting a nested array into a single array.
+
+const customFlatten = (arr, depth = 1) => {
+    let result = [];
+    arr.forEach(item => {
+        if (Array.isArray(item) && depth > 0) {
+            result = result.concat(customFlatten(item, depth - 1));
+        } else {
+            result.push(item);
+        }
+    })
+
+    return result;
+}
+
+let arr = [1, 2, [3, 4, [5, 6]]];
+
+console.log(customFlatten(arr, 2));
