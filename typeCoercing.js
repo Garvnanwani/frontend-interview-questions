b87151c2919e77453de608233019508d5ce61f7f
garv nanwani <garvwashere@gmail.com>
2022-03-29 21:12:30 +0530

    added type coercing questions

---
+++ typeCoercing.js
@@ -0,0 +1,9 @@
+// give the outputs for following logs
+
+console.log (undefined + 5) // Nan
+console.log ('undefined' + 5) // undefined5
+console.log ( 5 + 'undefined' ) // 5undefined
+console.log ( 5+ undefined)  // NaN
+console.log(0.1 + 0.2) // 0.30000000000000004
+console.log(0.1 + 0.2 == 0.3) // false
+console.log('b'+'a'+ +'a'+'a') // baNaNa
